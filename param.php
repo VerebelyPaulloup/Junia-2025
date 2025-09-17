2aa7728dbc463bd7aeadade60b0492ec55ee00a8
VerebelyPaulloup <verebelyi.polo@gmail.com>
2025-09-17 09:37:51 +0200

    Fix param.php

--- param.php
+++ param.php
@@ -1,11 +1,11 @@
 <?php
-	$base = 'site';
-	$pass = 'mnVkrECAWnj96Yk4';
-	$user = 'root';
-	$host = 'localhost';
-
-	//$base = 'qdeclercq';
-	//$pass = '90su39vi';
-	//$user = 'qdeclercq';
+	//$base = 'site';
+	//$pass = 'mnVkrECAWnj96Yk4';
+	//$user = 'root';
 	//$host = 'localhost';
+
+	$base = 'croupier';
+	$pass = '112904';
+	$user = 'junia';
+	$host = 'localhost';
 ?>
